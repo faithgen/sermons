150cb70df3220d5f0caeb433972c6d8f359ecf02
innoflash <innocentmazando@gmail.com>
2019-10-25 11:45:34 +0200

    moved sermon model

---
+++ src/Models/Sermon.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace FaithGen\Sermons\Models\Ministry;
+
+use FaithGen\SDK\Traits\TitleTrait;
+use FaithGen\SDK\Models\UuidModel;
+use FaithGen\SDK\Traits\Relationships\Belongs\BelongsToMinistryTrait;
+use FaithGen\SDK\Traits\Relationships\Morphs\CommentableTrait;
+use FaithGen\SDK\Traits\Relationships\Morphs\ImageableTrait;
+use FaithGen\SDK\Traits\StorageTrait;
+
+class Sermon extends UuidModel
+{
+    use  CommentableTrait, BelongsToMinistryTrait, StorageTrait, TitleTrait, ImageableTrait;
+
+    protected $casts = [
+        'main_verses' => 'array',
+        'reference_verses' => 'array',
+    ];
+
+    //****************************************************************************//
+    //***************************** MODEL ATTRIBUTES *****************************//
+    //****************************************************************************//
+    function getPreacherAttribute($val)
+    {
+        return ucwords($val);
+    }
+
+    function getSermonAttribute($val)
+    {
+        return ucfirst($val);
+    }
+    //****************************************************************************//
+    //***************************** MODEL ATTRIBUTES *****************************//
+    //****************************************************************************//
+
+    function formatVerse($verse)
+    {
+        ucwords($verse);
+        return true;
+    }
+
+    function filesDir()
+    {
+        return 'sermons';
+    }
+
+    function getFileName()
+    {
+        return $this->image->name;
+    }
+}
