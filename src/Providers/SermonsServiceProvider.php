2544e944f9e268d89225716e741bb4b540d92305
innoflash <innocentmazando@gmail.com>
2019-10-25 12:41:27 +0200

    readied config

--- src/Providers/SermonsServiceProvider.php
+++ src/Providers/SermonsServiceProvider.php
@@ -15,6 +15,7 @@ class SermonsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->mergeConfigFrom(__DIR__.'/../config/faithgen-sermons.php', 'faithgen-sermons');
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../database/migrations/' => database_path('migrations')
@@ -23,6 +24,10 @@ class SermonsServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../storage/sermons/' => storage_path('app/public/sermons')
             ], 'faithgen-sermons-storage');
+
+            $this->publishes([
+                __DIR__.'/../config/faithgen-sermons.php' => config_path('faithgen-sermons.php')
+            ], 'faithgen-sermons-config');
         }
 
         Sermon::observe(SermonObserver::class);
---
+++ src/config/faithgen-sermons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    /**
+     * The prefix to your sermons routes
+     */
+    'prefix' => 'sermons'
+];
