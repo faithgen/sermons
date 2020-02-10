cfd7b7cc45435b41eefc728285e8a0bc642869c6
innoflash <innocentmazando@gmail.com>
2020-02-10 17:55:27 +0200

    added dependancies

--- src/routes/source.php
+++ src/routes/source.php
@@ -6,9 +6,12 @@ use Illuminate\Support\Facades\Route;
 /**
  * Handles sermons details
  */
-Route::name('sermons.')->prefix('sermons/')->group(function () {
-    Route::post('create', [SermonController::class, 'create'])->middleware('source.site');
-    Route::delete('delete', [SermonController::class, 'delete'])->middleware('source.site');
-    Route::post('/update-picture', [SermonController::class, 'updatePicture'])->middleware('source.site');
-    Route::post('/update', [SermonController::class, 'update'])->middleware('source.site');
-});
+Route::name('sermons.')
+    ->prefix('sermons/')
+    ->middleware('source.site')
+    ->group(function () {
+        Route::post('create', [SermonController::class, 'create']);
+        Route::delete('delete', [SermonController::class, 'delete']);
+        Route::post('/update-picture', [SermonController::class, 'updatePicture']);
+        Route::post('/update', [SermonController::class, 'update']);
+    });
