08dd036cf92be707cec9b5b0afdb59808beb3840
innoflash <innocentmazando@gmail.com>
2020-02-18 21:57:54 +0200

    fetching comments

--- src/Http/Controllers/SermonController.php
+++ src/Http/Controllers/SermonController.php
@@ -111,7 +111,7 @@ class SermonController extends Controller
 
     public function comments(Request $request, Sermon $sermon)
     {
-        $this->authorize('sermon.view', $sermon);
+        $this->authorize('view', $sermon);
         return CommentHelper::getComments($sermon, $request);
     }
 }
