585e053c7395afdab52b7d938ee9e13ee328b143
innoflash <innocentmazando@gmail.com>
2020-05-05 12:28:49 +0200

    added s3 upload

--- src/Jobs/S3Upload.php
+++ src/Jobs/S3Upload.php
@@ -2,24 +2,30 @@
 
 namespace FaithGen\Sermons\Jobs;
 
+use FaithGen\SDK\Traits\SavesToAmazonS3;
 use FaithGen\Sermons\Models\Sermon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class S3Upload implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable,
+        InteractsWithQueue,
+        Queueable,
+        SerializesModels,
+        SavesToAmazonS3;
 
-    public $deleteWhenMissingModels = true;
-    protected $sermon;
+    public bool $deleteWhenMissingModels = true;
+    protected Sermon $sermon;
 
     /**
      * Create a new job instance.
      *
-     * @return void
+     * @param Sermon $sermon
      */
     public function __construct(Sermon $sermon)
     {
@@ -33,6 +39,10 @@ class S3Upload implements ShouldQueue
      */
     public function handle()
     {
-        //
+        try {
+            $this->saveFiles($this->sermon);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
     }
 }
