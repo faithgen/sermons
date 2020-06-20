4aee43fbbafadda40fd07192c120812c03bcbb76
innoflash <innocentmazando@gmail.com>
2020-06-20 05:24:09 +0200

    updated creating

--- routes/source.php
+++ routes/source.php
@@ -10,7 +10,7 @@ Route::name('sermons.')
     ->prefix('sermons/')
     ->middleware('source.site')
     ->group(function () {
-        Route::post('create', [SermonController::class, 'create']);
+        Route::post('', [SermonController::class, 'create']);
         Route::delete('delete', [SermonController::class, 'delete']);
         Route::post('/update-picture', [SermonController::class, 'updatePicture']);
         Route::post('/update', [SermonController::class, 'update']);
